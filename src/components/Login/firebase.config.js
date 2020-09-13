5cec96eed1e51c1de1d981698374ebe5cd35eba0
mahmudurrahman75 <mahmudurrahmance75@gmail.com>
2020-09-13 13:21:03 +0600

    coding for burj-al-arab-master

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaMvVP6ihtHNVE7y6eyOoqZwFCKXuXxYk",
+    authDomain: "burj-al-arab-2b3f9.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-2b3f9.firebaseio.com",
+    projectId: "burj-al-arab-2b3f9",
+    storageBucket: "burj-al-arab-2b3f9.appspot.com",
+    messagingSenderId: "539166784748",
+    appId: "1:539166784748:web:f38c238b671899ad7fb9c9"
+  };
+  export default firebaseConfig;
